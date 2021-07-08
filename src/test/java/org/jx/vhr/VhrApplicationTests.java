ed6e00756a16c10e3070691b1b287505c3563011
123 <chenjiuxiang1994@gmail.com>
2021-07-08 07:46:56 -0400

    Revert login""""

---
+++ src/test/java/org/jx/vhr/VhrApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jx.vhr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VhrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
